41b18347b613451baa93f783ec210e8e8413562a
valevurro <vale.v.00@outlook.it>
2025-09-23 20:31:53 +0200

    Refactor AppHeader component layout and enhance CSS styles for better responsiveness

--- my-react-d-comics/src/components/AppHeader.jsx
+++ my-react-d-comics/src/components/AppHeader.jsx
@@ -3,19 +3,19 @@ import dcLogo from '../assets/img/dc-logo.png'
 export default function AppHeader() {
     return (
         <header>
-            <div className='container d-flex align-items-center p-2'>
+            <div className='container'>
                 <img src={dcLogo} alt="" />
-                <nav className='ms-auto ms-5'>
+                <nav className='mt-10'>
                     <a href="">Characters</a>
-                    <a href="">Characters</a>
-                    <a href="">Characters</a>
-                    <a href="">Characters</a>
-                    <a href="">Characters</a>
-                    <a href="">Characters</a>
-                    <a href="">Characters</a>
-                    <a href="">Characters</a>
-                    <a href="">Characters</a>
-                    <a href="">Characters</a>
+                    <a href="">Comics</a>
+                    <a href="">Movies</a>
+                    <a href="">TV</a>
+                    <a href="">Games</a>
+                    <a href="">Collectibles</a>
+                    <a href="">Videos</a>
+                    <a href="">Fans</a>
+                    <a href="">News</a>
+                    <a href="">Shop</a>
                 </nav>
             </div>
         </header>
