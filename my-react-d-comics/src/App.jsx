9edfd3e0b8921de34b5866548d7806acdfe93eab
valevurro <vale.v.00@outlook.it>
2025-09-24 22:04:43 +0200

    Add AppFooter component and integrate it into the App layout

--- my-react-d-comics/src/App.jsx
+++ my-react-d-comics/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import AppHeader from './components/AppHeader'
 import AppMain from './components/AppMain'
+import AppFooter from './components/AppFooter'
 
 import '@fontsource-variable/open-sans';
 
@@ -13,6 +14,7 @@ function App() {
     <>
       <AppHeader />
       <AppMain />
+      <AppFooter />
     </>
   )
 }
---
+++ my-react-d-comics/src/components/AppFooter.jsx
@@ -0,0 +1,8 @@
+import facebookIcon from '../assets/img/footer-facebook.png';
+import twitterIcon from '../assets/img/footer-twitter.png';
+import youtubeIcon from '../assets/img/footer-youtube.png';
+import pinterestIcon from '../assets/img/footer-pinterest.png';
+import periscopeIcon from '../assets/img/footer-periscope.png';
+import footerBg from '../assets/img/footer-bg.jpg';
+
+
