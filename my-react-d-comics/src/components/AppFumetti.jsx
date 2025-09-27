5a9a2605f084ef1e4a9c8f26031d812c09002007
valevurro <vale.v.00@outlook.it>
2025-09-27 12:03:21 +0200

    Add AppFumetti

---
+++ my-react-d-comics/src/components/AppFumetti.jsx
@@ -0,0 +1,19 @@
+export default function AppFumetti({ thumb, series, children }) {
+    return (
+        <>
+            {thumb && (
+                <img src={thumb} alt={series} className="img-fluid" style={{ width: "200px", height: "200px", objectFit: "cover" }} />
+            )
+            }
+            <div>
+                <h6 className="text-light text-break text-uppercase pt-3 pb-2 text-start" style={{ whiteSpace: "normal", overflowWrap: "break-word", fontSize: "12px" }}>
+                    <a href="" className="text-decoration-none" target="_blank" rel="noopener noreferrer">
+                        {series}
+                    </a>
+                </h6>
+                {children}
+            </div>
+
+        </>
+    )
+}
